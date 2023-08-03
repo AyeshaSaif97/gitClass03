52fa1a8d9b47d538e65f7203dfdb8d8d84f25f9e
AyeshaSaif97 <ayesha.saif13@gmail.com>
2023-08-02 20:13:37 -0400

    3rd commit

---
+++ src/Solarsystem.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class Solarsystem {
+    public static void main(String[] args) {
+
+
+        System.out.println("Code is complete");
+        Solarsystem Solarsystem = new Solarsystem();
+
+
+
+    }
+}
